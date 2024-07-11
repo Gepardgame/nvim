6d69643b2b2fc9e8583f96a367cedc93f8d3e9a7
Gepardgame <75749982+Gepardgame@users.noreply.github.com>
2024-07-11 11:56:42 +0200

    Foramting

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,23 +1,23 @@
 return {
-	{
-		"f-person/git-blame.nvim",
-		config = function()
-			vim.keymap.set("n", "<leader>gb", "<cmd>GitBlameToggle<cr>", { desc = "Toggles git blame line in lualine" })
-			vim.g.gitblame_display_virtual_text = 0
-		end,
-	},
-	{
-		"NeogitOrg/neogit",
-		tag = "v0.0.1",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"sindrets/diffview.nvim",
+  {
+    "f-person/git-blame.nvim",
+    config = function()
+      vim.keymap.set("n", "<leader>gb", "<cmd>GitBlameToggle<cr>", { desc = "Toggles git blame line in lualine" })
+      vim.g.gitblame_display_virtual_text = 0
+    end,
+  },
+  {
+    "NeogitOrg/neogit",
+    tag = "v0.0.1",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
 
-			"nvim-telescope/telescope.nvim",
-		},
-		config = function()
-			require("neogit").setup({})
-			vim.keymap.set("n", "<leader>gg", "<cmd>Neogit<cr>", { desc = "Opens git window" })
-		end,
-	},
+      "nvim-telescope/telescope.nvim",
+    },
+    config = function()
+      require("neogit").setup({})
+      vim.keymap.set("n", "<leader>gg", "<cmd>Neogit<cr>", { desc = "Opens git window" })
+    end,
+  },
 }
