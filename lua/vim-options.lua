5ec41e5626b25df1a904e48d552dacf10725e9f8
Gepardgame <thomas.degg@gmail.com>
2024-07-11 11:47:49 +0200

    Improved some things and added shortcuts

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,18 +3,25 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
-vim.o.clipboard = "unnamedplus"
+vim.g.autoformat = true
+vim.o.clipboard = vim.env.SSH_TTY and "" or "unnamedplus"
+vim.conceallevel = 2
+vim.confirm = true
+vim.cursorline = true
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.ttimeoutlen = 100
-
+vim.keymap.set("n", "<leader>h", "<cmd>nohlsearch<cr>", { desc = "Clear search Highlighting" })
+vim.keymap.set("n", "<leader>l", "<cmd>Lazy<cr>", { desc = "Lazy" })
 vim.keymap.set({ "n", "v", "i" }, "<C-s>", function()
-	vim.cmd("w")
-	vim.lsp.buf.format()
-	vim.cmd("w")
-end, {})
-vim.keymap.set({ "n", "v", "i" }, "<C-S>", function()
-	vim.cmd("wa")
-	vim.lsp.buf.format()
-	vim.cmd("wa")
-end, {})
+  vim.cmd("w")
+  vim.lsp.buf.format()
+  vim.cmd("w")
+  vim.api.nvim_command("stopinsert")
+end, { desc = "Save File" })
+vim.keymap.set({ "n", "v", "i" }, "<C-m-s>", function()
+  vim.cmd("wa")
+  vim.lsp.buf.format()
+  vim.cmd("wa")
+  vim.api.nvim_command("stopinsert")
+end, { desc = "Save all Files" })
